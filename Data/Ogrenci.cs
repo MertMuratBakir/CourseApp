c91d031ea76acc8a27bdf432654c5fabe5a239b2
Mert Murat Bakır <125885626+MertMuratBakir@users.noreply.github.com>
2025-02-07 16:25:15 +0300

    Create Ogrenci.cs

---
+++ Data/Ogrenci.cs
@@ -0,0 +1,10 @@
+namespace CourseApp.Data
+{
+    public class Ogrenci
+    {
+        public int Id { get; set; }
+        public string? Ad { get; set; }
+        public string? Soyad { get; set; }
+        public int OgrenciId { get; set; }
+    }
+}
