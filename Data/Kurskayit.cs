d74ae7bf2f3728ffeecc7ef4d41cc4380053ed53
Mert Murat Bakır <125885626+MertMuratBakir@users.noreply.github.com>
2025-02-07 16:25:12 +0300

    Create Kurskayit.cs

---
+++ Data/Kurskayit.cs
@@ -0,0 +1,9 @@
+namespace CourseApp.Data
+{
+    public class KursKayit
+    {
+        public int Id { get; set; }
+        public int OgrenciId { get; set; }
+        public int KursId { get; set; }
+    }
+}
