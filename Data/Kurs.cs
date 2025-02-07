a3b799e7f5e717dfc8898dcf88099836ece61bb2
Mert Murat Bakır <125885626+MertMuratBakir@users.noreply.github.com>
2025-02-07 16:25:09 +0300

    Create Kurs.cs

---
+++ Data/Kurs.cs
@@ -0,0 +1,9 @@
+namespace CourseApp.Data
+{
+    public class Kurs
+    {
+        public int Id { get; set; }
+        public string? KursAdi { get; set; }
+        public int KursId { get; set; }
+    }
+}
