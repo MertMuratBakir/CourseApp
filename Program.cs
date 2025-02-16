58ca476e2a59186ae9ebcc0a4e67f4d9a32214ab
Mert Murat Bakır <125885626+MertMuratBakir@users.noreply.github.com>
2025-02-16 17:11:17 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DataContext>(options => {
+    options.UseSqlite();
+});
 
 var app = builder.Build();
 
