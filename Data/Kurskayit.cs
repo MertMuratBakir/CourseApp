cc68672faa8986845f52843e0b7f8186047cff78
Mert Murat Bakır <125885626+MertMuratBakir@users.noreply.github.com>
2025-02-16 18:08:06 +0300

    Update Kurskayit.cs

--- Data/Kurskayit.cs
+++ Data/Kurskayit.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CourseApp.Data
 {
     public class KursKayit
     {
+        [Key]
         public int KayitId { get; set; }
         public int OgrenciId { get; set; }
         public int KursId { get; set; }
