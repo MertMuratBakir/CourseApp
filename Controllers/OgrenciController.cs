039ed885651b16dacc15424c933157cd3d2623aa
Mert Murat Bakır <125885626+MertMuratBakir@users.noreply.github.com>
2025-02-17 20:48:24 +0300

    Update OgrenciController.cs

--- Controllers/OgrenciController.cs
+++ Controllers/OgrenciController.cs
@@ -1,12 +1,28 @@
+using CourseApp.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CourseApp.Controllers 
 {
     public class OgrenciController: Controller
     {
+
+        private readonly DataContext _context;
+
+        public OgrenciController(DataContext context)
+        {
+            _context = context;
+        }
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Ogrenci model)
+        {
+            _context.Ogrenciler.Add(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
