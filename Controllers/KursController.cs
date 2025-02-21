398e4c6fa71ce08385925ecafb04ba8bc6b881b8
Mert Murat Bakır <125885626+MertMuratBakir@users.noreply.github.com>
2025-02-21 21:47:49 +0300

    Update KursController.cs

--- Controllers/KursController.cs
+++ Controllers/KursController.cs
@@ -22,6 +22,7 @@ namespace CourseApp.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Kurs model)
         {
             _context.Kurslar.Add(model);
@@ -29,5 +30,73 @@ namespace CourseApp.Controllers
             return RedirectToAction("Index", "Kurs");
         }
 
-    }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var kurs = await _context.Kurslar.FindAsync(id);
+
+                if (kurs == null)
+                {
+                    return NotFound();
+                }
+                return View(kurs);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Kurs Model)
+        {
+            if (id != Model.KursId)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _context.Update(Model);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Kurs");
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var Kurs = await _context.Kurslar.FindAsync(id);
+
+                if (Kurs == null)
+                {
+                    return NotFound();
+                }
+                return View(Kurs);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete([FromForm] int id)
+        {
+            var Kurs = await _context.Kurslar.FindAsync(id);
+            if (Kurs == null)
+            {
+                return NotFound();
+            }
+            _context.Kurslar.Remove(Kurs);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Kurs");
+        }
+
+
+    } 
 }
