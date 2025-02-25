f7f139d8ddf9f3d25a22e8a1ec6bb5dc5b5d72c8
Mert Murat Bakır <125885626+MertMuratBakir@users.noreply.github.com>
2025-02-25 15:24:25 +0300

    Update Kurskayit.cs

--- Data/Kurskayit.cs
+++ Data/Kurskayit.cs
@@ -8,5 +8,6 @@ namespace CourseApp.Data
         public int KayitId { get; set; }
         public int OgrenciId { get; set; }
         public int KursId { get; set; }
+        public DateTime KayitTarihi { get; set; }
     }
 }
