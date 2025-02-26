bcb62a770321c546ee1d9908a010d7153b88242a
Mert Murat Bakır <125885626+MertMuratBakir@users.noreply.github.com>
2025-02-26 18:35:22 +0300

    Create OgretmenController.cs

---
+++ Controllers/OgretmenController.cs
@@ -0,0 +1,126 @@
+using CourseApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseApp.Controllers
+{
+    public class OgretmenController : Controller
+    {
+        private readonly DataContext _context;
+        public OgretmenController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            var Ogretmenler = await _context.Ogretmenler.ToListAsync();
+            return View(Ogretmenler);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Ogretmen model)
+        {
+            _context.Ogretmenler.Add(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Ogretmen");
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var ogr = await _context
+                                .Ogretmenler
+                                .Include(o => o.Kurslar)
+                                .ThenInclude(o => o.KursKayitlari)
+                                .FirstOrDefaultAsync(o => o.OgretmenId == id);
+
+                if (ogr == null)
+                {
+                    return NotFound();
+                }
+                return View(ogr);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Ogretmen Model)
+        {
+            if (id != Model.OgretmenId)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(Model);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Ogretmenler.Any(o => o.OgretmenId == Model.OgretmenId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index", "Ogretmen");
+            }
+
+            return View(Model);
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var ogretmen = await _context.Ogretmenler.FindAsync(id);
+
+                if (ogretmen == null)
+                {
+                    return NotFound();
+                }
+                return View(ogretmen);
+            }
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete([FromForm] int id)
+        {
+            var ogretmen = await _context.Ogretmenler.FindAsync(id);
+            if (ogretmen == null)
+            {
+                return NotFound();
+            }
+            _context.Ogretmenler.Remove(ogretmen);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
