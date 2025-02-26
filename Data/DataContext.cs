80ce549d1361643fdbcb1f881d0952823a8c363c
Mert Murat Bakır <125885626+MertMuratBakir@users.noreply.github.com>
2025-02-26 18:35:25 +0300

    Update DataContext.cs

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -4,11 +4,13 @@ namespace CourseApp.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options) {
-            
-         }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
         public DbSet<Kurs> Kurslar => Set<Kurs>();
         public DbSet<Ogrenci> Ogrenciler => Set<Ogrenci>();
         public DbSet<KursKayit> Kurskayitlari => Set<KursKayit>();
-    };
+        public DbSet<Ogretmen> Ogretmenler => Set<Ogretmen>();
+    }
 }
